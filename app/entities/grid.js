7d8babd839fcacb81590a04ca1b616d0d0178320
Javier Alvarez <javier.alvarez@taptapnetworks.com>
2013-10-17 18:13:18 +0200

    stuff

--- app/entities/grid.js
+++ app/entities/grid.js
@@ -23,27 +23,29 @@ Crafty.c('Grid', {
     this.attr({
       w: x,
       h: y
-    })
+    });
     return this;
   },
   toRadians: function(degrees) {
-    return degrees * Math.PI / 180
+    return degrees * Math.PI / 180;
   },
 
   rotatedPosition: function(displacement) {
-    displacement = displacement || [0,0]
+    displacement = displacement || [0,0];
     var cosHeading = (Math.cos(this.toRadians(this.heading)));
     var sinHeading = (Math.sin(this.toRadians(this.heading)));
     return [
-        this.x + displacement[0] * cosHeading - displacement[1] * sinHeading,
-        this.y + displacement[0] * sinHeading + displacement[1] * cosHeading
-      ]
+      this.x + displacement[0] * cosHeading - displacement[1] * sinHeading,
+      this.y + displacement[0] * sinHeading + displacement[1] * cosHeading
+    ];
   },
   distanceTo: function(entity) {
-    return Crafty.math.distance(this.x, this.y, entity.x, entity.y)
+    return Crafty.math.distance(this.x, this.y, entity.x, entity.y);
   },
   getAngleTo: function(entity) {
-    var angle = Math.atan2(entity.y - this.y, entity.x - this.x);
+    var x = entity.x || entity[0];
+    var y = entity.y || entity[1];
+    var angle = Math.atan2(y - this.y, x - this.x);
     if (angle < 0) angle = Math.PI * 2 + angle;
     return Crafty.math.radToDeg(angle) % 360;
   }
